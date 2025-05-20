2e6cf01e5d3cfeba9c557cfc6a7ebbd383fe6e46
LucasR1006 <lucasor1006@gmail.com>
2025-05-19 21:18:06 -0300

    ej1 hecho

--- ejercicios/ejercicio1.go
+++ ejercicios/ejercicio1.go
@@ -7,5 +7,19 @@ type Actividad struct {
 }
 
 func SelectorRecursivo(actividades []Actividad) []Actividad {
-	panic("No implementado")
+	pepe2 := make([]Actividad, 0)
+
+	pepe2 = append(pepe2, actividades[0])
+
+	return Selector(actividades[1:], pepe2)
+}
+
+func Selector(actividades []Actividad, array []Actividad) []Actividad {
+	if len(actividades) == 1 {
+		return append(array, actividades[0])
+	}
+	if actividades[0].Inicio >= array[len(array)-1].Fin {
+		array = append(array, actividades[0])
+	}
+	return Selector(actividades[1:], array)
 }
