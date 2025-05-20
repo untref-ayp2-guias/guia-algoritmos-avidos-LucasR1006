e90bb95cff9b884615198a72afdd81ed24c13b85
LucasR1006 <lucasor1006@gmail.com>
2025-05-19 21:20:43 -0300

    ej3 hecho

--- ejercicios/ejercicio3.go
+++ ejercicios/ejercicio3.go
@@ -1,5 +1,7 @@
 package ejercicios
 
+import "sort"
+
 type Objeto struct {
 	Nombre string
 	Peso   int
@@ -7,5 +9,25 @@ type Objeto struct {
 }
 
 func Ejercicio3(objetos []Objeto, capacidad int) map[Objeto]float32 {
-	panic("No implementado")
+	resultado := make(map[Objeto]float32)
+	//Ordenamos de mayor a menor
+	sort.Slice(objetos, func(i, j int) bool {
+		return float32(objetos[i].Valor)/float32(objetos[i].Peso) > float32(objetos[j].Valor)/float32(objetos[j].Peso)
+	})
+
+	capacidadRestante := capacidad
+	for _, obj := range objetos {
+		if capacidadRestante == 0 {
+			break
+		}
+		if obj.Peso <= capacidadRestante {
+			capacidadRestante -= obj.Peso
+			resultado[obj] = 1.0
+		} else {
+			fraccion := float32(capacidadRestante) / float32(obj.Peso)
+			resultado[obj] = fraccion
+			capacidadRestante = 0
+		}
+	}
+	return resultado
 }
